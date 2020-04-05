3dda0c967ea61f6cc6e11be6a7f3e46b5f31408b
leandroromano <leandrolromano@gmail.com>
2020-04-05 01:05:56 -0300

    Refactor to Search feature

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -4,39 +4,42 @@ import { PokemonsService, Pokemon } from '../../services/pokemons.service';
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
 })
 export class SearchComponent implements OnInit {
   pokemon: Pokemon;
   infoSelected: boolean = false;
   abilitiesSelected: boolean = false;
   evolutionsSelected: boolean = false;
+  showWarning: boolean = false;
   searchInput: string;
   constructor(private _pokemonsService: PokemonsService) {}
 
   ngOnInit(): void {}
 
-  handleInfoClick(): void {
-    this.pokemon = this._pokemonsService.getPokemon(this.searchInput);
-    this.setAllFalse();
-    this.infoSelected = !this.infoSelected;
-  }
-
-  handleAbilitiesClick(): void {
-    this.pokemon = this._pokemonsService.getPokemon(this.searchInput);
-    this.setAllFalse();
-    this.abilitiesSelected = !this.abilitiesSelected;
-  }
-
-  handleEvolutionsClick(): void {
-    this.pokemon = this._pokemonsService.getPokemon(this.searchInput);
-    this.setAllFalse();
-    this.evolutionsSelected = !this.evolutionsSelected;
-  }
-
   setAllFalse(): void {
     this.infoSelected = false;
     this.abilitiesSelected = false;
     this.evolutionsSelected = false;
+  }
+
+  handleClickTable(selectedButton: string): void {
+    this.pokemon = this._pokemonsService.getPokemon(this.searchInput);
+    this.setAllFalse();
+    if (this.pokemon === undefined) {
+      this.showWarning = true;
+    } else {
+      this.showWarning = false;
+      switch (selectedButton) {
+        case 'info':
+          this.infoSelected = true;
+          break;
+        case 'abilities':
+          this.abilitiesSelected = true;
+          break;
+        case 'evolutions':
+          this.evolutionsSelected = true;
+      }
+    }
   }
 }
