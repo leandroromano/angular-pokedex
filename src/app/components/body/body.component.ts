b69b921caa9f452f55cb47f18d75add87cc0e558
leandroromano <leandrolromano@gmail.com>
2020-06-04 17:40:10 -0300

    Angular animation fade in

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -1,7 +1,16 @@
 import { Component } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-body',
-  templateUrl: 'body.component.html'
+  templateUrl: 'body.component.html',
+  animations: [
+    trigger('fadeIn', [
+      transition('void => *', [
+        style({ opacity: 0 }),
+        animate(2000)
+      ])
+    ])
+  ]
 })
-export class BodyComponent {}
+export class BodyComponent { }
